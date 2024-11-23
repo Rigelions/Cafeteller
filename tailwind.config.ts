da49b649a046c476d56e0996461da12409d8ae99
realkyr <phuree.kanusont@gmail.com>
2024-11-24 00:01:09 +0700

    style(tw): remove theme use extend instead

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from 'tailwindcss'
-import colors from 'tailwindcss/colors'
 
 const config: Config = {
   important: true,
@@ -15,29 +14,28 @@ const config: Config = {
       contain: 'contain',
       '100%': '100%'
     },
-    colors: {
-      ...colors,
-      primary: '#1e315c',
-      secondary: '#c7bdb3',
-      icons: {
-        default: '#ceba84',
-        active: '#1e315c',
-        nav: '#ab9e91'
-      },
-      cream: {
-        100: '#e5e7eb',
-        200: '#f1f5f8',
-        300: '#f9fafb',
-        400: '#fafafa',
-        500: '#ffffff',
-        600: '#ffffff'
-      }
-    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      colors: {
+        primary: '#1e315c',
+        secondary: '#c7bdb3',
+        icons: {
+          default: '#ceba84',
+          active: '#1e315c',
+          nav: '#ab9e91'
+        },
+        cream: {
+          100: '#e5e7eb',
+          200: '#f1f5f8',
+          300: '#f9fafb',
+          400: '#fafafa',
+          500: '#ffffff',
+          600: '#ffffff'
+        }
       }
     }
   },
